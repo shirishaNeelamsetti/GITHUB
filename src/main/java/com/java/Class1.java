2665ee02a3195d1f7b0a67c8b218c43c1d6e916a
shirishaNeelamsetti <shirishaneelamsetti04@gmail,com>
2024-10-03 12:47:06 +0530

    first commit

---
+++ src/main/java/com/java/Class1.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+System.out.println("hi hello im shirisha");
+System.out.println("hello manisha");
+	}
+
+}
---
+++ src/main/java/com/java/Class2.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+System.out.println("my father name is atchi babu");
+System.out.println("my mother name is shanthi");
+	}
+
+}
